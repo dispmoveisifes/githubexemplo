546ce2979b54429611d09420e0bf170abc82403e
danielrt <danielrt@ifes.edu.br>
2020-11-27 18:00:12 -0300

    Commit inicial do projeto

---
+++ app/src/main/java/com/example/githubexemplo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.githubexemplo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
